c2c745713c6dca3cdde11a01205b461f9cda819c
Max Shishkov <maxshishkov92@gmail.com>
2025-12-30 20:37:41 -0600

    feat: add shooting mechanics

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_COLOR, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
-
+from constants import PLAYER_RADIUS, PLAYER_COLOR, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -34,9 +34,18 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
             
     def move(self, dt):
         unit_vectore = pygame.Vector2(0,1)
         rotated_vector = unit_vectore.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+        
+    def shoot(self):
+        bullet = Shot(self.position.x, self.position.y)
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vectore = rotated_vector * PLAYER_SHOOT_SPEED
+        bullet.set_velocity(rotated_with_speed_vectore)
