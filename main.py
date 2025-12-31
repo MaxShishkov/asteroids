d84c394e009c3446b572454d89067593334c6217
Max Shishkov <maxshishkov92@gmail.com>
2025-12-30 19:03:14 -0600

    feat: update for drawing player movements

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    clock = pygame.time.Clock()
+    dt = 0
+    player_x = SCREEN_WIDTH / 2
+    player_y = SCREEN_HEIGHT / 2
+    player = Player(player_x, player_y)
     
-    while True:
+    while running:
         log_state()
-        screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        player.update(dt)
+        
+        screen.fill("black")       
+        player.draw(screen)
+        
+                
         pygame.display.flip()
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        # end of iteration
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
