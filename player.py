a9fea93da08f0ed2cf112f80ec9fdfa6112ca3a7
Max Shishkov <maxshishkov92@gmail.com>
2025-12-30 19:02:46 -0600

    feat: add ability to move

--- player.py
+++ player.py
@@ -1,7 +1,6 @@
-from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_COLOR, LINE_WIDTH
-
 import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, PLAYER_COLOR, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 
 class Player(CircleShape):
@@ -11,7 +10,7 @@ class Player(CircleShape):
         self.rotation = 0
         
     def triangle(self):
-        forward = pygame.Vector2(0, 2).rotate(self.rotation)
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
@@ -20,3 +19,24 @@ class Player(CircleShape):
     
     def draw(self,screen):
         pygame.draw.polygon(screen, PLAYER_COLOR, self.triangle(), LINE_WIDTH)
+        
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+            
+    def move(self, dt):
+        unit_vectore = pygame.Vector2(0,1)
+        rotated_vector = unit_vectore.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
