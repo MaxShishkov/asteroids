abe61aa70d9328d63759f287c97b1199329d9c4a
Max Shishkov <maxshishkov92@gmail.com>
2025-12-30 20:44:26 -0600

    add bullet rate limit

--- player.py
+++ player.py
@@ -1,13 +1,22 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_COLOR, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
 from shot import Shot
+from constants import (
+    PLAYER_RADIUS,
+    PLAYER_COLOR,
+    LINE_WIDTH,
+    PLAYER_TURN_SPEED,
+    PLAYER_SPEED,
+    PLAYER_SHOOT_SPEED,
+    PLAYER_SHOOT_COOLDOWN_SECONDS,
+)
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         
         self.rotation = 0
+        self.shot_cooldown = 0
         
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -24,6 +33,7 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
         
     def update(self, dt):
+        self.shot_cooldown -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -44,8 +54,10 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
         
     def shoot(self):
-        bullet = Shot(self.position.x, self.position.y)
-        unit_vector = pygame.Vector2(0, 1)
-        rotated_vector = unit_vector.rotate(self.rotation)
-        rotated_with_speed_vectore = rotated_vector * PLAYER_SHOOT_SPEED
-        bullet.set_velocity(rotated_with_speed_vectore)
+        if self.shot_cooldown <= 0:
+            bullet = Shot(self.position.x, self.position.y)
+            unit_vector = pygame.Vector2(0, 1)
+            rotated_vector = unit_vector.rotate(self.rotation)
+            rotated_with_speed_vectore = rotated_vector * PLAYER_SHOOT_SPEED
+            bullet.set_velocity(rotated_with_speed_vectore)
+            self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
